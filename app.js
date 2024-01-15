c7fc52591d62a390e79460465cf0c7bfdf05274d
WMZ_dev <2751823717@qq.com>
2024-01-15 20:50:24 +0800

    拿到post请求参数-body-parser 以及 添加接口

--- app.js
+++ app.js
@@ -1,12 +1,59 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
+app.use(bodyParser.json());
 const port = 4747
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.get('/api/projects', (req, res) => {
-    res.send('Hello World!/api/projects')
+    let data = {
+        code: 0,
+        project_list: [{
+            key: '1',
+            projectName: '测试项目',
+            section: '工程部',
+            owner: '黄晓明',
+            createTime: '2014-01-01',
+        }]
+    }
+    res.send(data)
+})
+app.post('/api/login', (req, res) => {
+    const reqBody = req.body
+    let data = {}
+    if (reqBody.username === '1') {
+        data = {
+            code: 0,
+            msg: 'ok'
+        }
+        res.send(data)
+        return
+    }
+    data = {
+        code: 402,
+        msg: '登陆失败'
+    }
+    res.send(data)
+})
+
+app.post('/api/register', (req, res) => {
+    const reqBody = req.body
+    let data = {}
+    if (reqBody.username === '1') {
+        data = {
+            code: 0,
+            msg: 'ok'
+        }
+        res.send(data)
+        return
+    }
+    data = {
+        code: 402,
+        msg: '注册失败'
+    }
+    res.send(data)
 })
 
 
